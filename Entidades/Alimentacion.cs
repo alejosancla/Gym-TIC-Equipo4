af2b7c90c2378a602380c2b09d90dceddf531a43
Jose Sanchez <alejotri99@hotmail.com>
2022-08-28 14:08:42 -0500

    version 1

---
+++ Entidades/Alimentacion.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Gimnasio.App.Dominio
+{
+    public class Alimentacion
+    {
+        public string Desayuno{get;set;}
+        public string Almuerzo{get;set;}
+        public string Cena{get;set;}
+        public string DiasSemana{get;set;}
+
+    }
+}
