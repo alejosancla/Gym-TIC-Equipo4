5ac99b5433d37fd546aa9d7604e3a2dcd0bc4f03
Jhonatan Rojas <111137856+RojasJhonatan@users.noreply.github.com>
2022-08-30 10:44:50 -0500

    cambios entidades

---
+++ GymTic.App.Dominio/Entidades/Categoria(Nutricion).cs
@@ -0,0 +1,10 @@
+using system;
+
+namespace GymTic.App.Dominio
+{
+    public class Categoria(Nutricion){
+        public string NombreCategoria{get;set;}
+        public int PuntosSaludables{get;set;}
+        
+    }
+}
---
+++ GymTic.App.Dominio/Entidades/Dieta.cs
@@ -0,0 +1,13 @@
+using system;
+
+namespace GymTic.App.Dominio
+{
+    public class Dieta{
+        public string Nombre{get;set;}
+        public Alimentacion Alimentacion{get;set;}
+        public int Duracion{get;set;}
+        public Categoria categoria{get;set;}
+        public string Verificacion{get;set;}
+
+    }
+}
