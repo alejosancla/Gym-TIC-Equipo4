6b4412173dbf9e6a796da8f6332b8d22fe312439
RojasJhonatan <jmr.85@hotmail.com>
2022-08-30 10:01:02 -0500

    Cambio nutricion

--- GymTic.App.Dominio/Entidades/Nutricion.cs
+++ GymTic.App.Dominio/Entidades/Nutricion.cs
@@ -1,7 +1,16 @@
 using system;
 
 namespace GymTic.App.Dominio{
-    public class Nutricion{
-    }
+    public class Nutricion
+    {
+        using system;
 
+namespace Gimnasio.App.Dominio
+    {
+        public class Nutricion
+        {
+        public Dieta Dieta{get;set;}
+        public int PuntosVerificacion{get;set;}
+        }
+    }
 }
