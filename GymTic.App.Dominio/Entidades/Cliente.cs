b3959e1784d9ace417534a7bcb1e4415820bcc30
RojasJhonatan <jmr.85@hotmail.com>
2022-08-30 15:35:32 -0500

    Primer commit

--- GymTic.App.Dominio/Entidades/Cliente.cs
+++ GymTic.App.Dominio/Entidades/Cliente.cs
@@ -3,7 +3,7 @@ using system;
 namespace GymTic.App.Dominio{
     public class Cliente
     {
-        
+      public string Nombre {get; set;}  
     }
 
 }
--- GymTic.App.Dominio/Entidades/Nutricion.cs
+++ GymTic.App.Dominio/Entidades/Nutricion.cs
@@ -1,16 +1,11 @@
 using system;
 
-namespace GymTic.App.Dominio{
-    public class Nutricion
-    {
-        using system;
+namespace GymTic.App.Dominio
+{
+   public class Nutricion{
+    public Dieta Dieta{get;set;}
+    public int PuntosVerificacion{get;set;}
+    
 
-namespace Gimnasio.App.Dominio
-    {
-        public class Nutricion
-        {
-        public Dieta Dieta{get;set;}
-        public int PuntosVerificacion{get;set;}
-        }
-    }
+   }
 }
